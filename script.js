eaf96ca1c9a7dbf4b39bbdaf259dcaaa947b2634
Chisom Laura <102640478+ChisomLaura@users.noreply.github.com>
2022-06-07 21:17:14 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var tempCelsius = 10;
+var toFarhenheit = tempCelsius * 1.8 + 32;
+
+console.log(
+  `Outside my friend's house, it's ${tempCelsius} degrees Celsius, which is ${toFarhenheit} degrees Fahrenheit.`
+);
